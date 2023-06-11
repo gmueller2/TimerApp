e991ade4a7a6a112185dcaf12903a9c72d7f144e
Gene Mueller <geneemueller@gmail.com>
2023-06-10 17:02:25 -0700

    final push?

--- TimerApp/ViewController.swift
+++ TimerApp/ViewController.swift
@@ -6,38 +6,91 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var currentTimeLabel: UILabel!
-    
+    @IBOutlet weak var timerButton: UIButton!
+    @IBOutlet weak var remainingTimeLabel: UILabel!
     @IBOutlet weak var timePicker: UIDatePicker!
     
+    
     var clock = Timer()
+    var timeRemaining = 0
+    var timerOn = false
+    var player: AVAudioPlayer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // gets the current time
         getCurrentTime()
-        Timer.scheduledTimer(timeInterval: 1.0, target:self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
         // Do any additional setup after loading the view.
     }
     
     private func getCurrentTime() {
+        // gets a current time for the clock itself on the top
         clock = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(self.currentTime), userInfo: nil, repeats: true)
     }
     
     @objc func currentTime() {
+        // formats the date
         let formatter = DateFormatter()
         formatter.dateFormat = "dd MMM yyyy HH:mm:ss"
+        // prints the date out on the label
         currentTimeLabel.text = Date().formatted(Date.FormatStyle().weekday(.abbreviated)) + ", " + formatter.string(from: Date())
+        changeBackground()
+        // call a function to reduce the countdown duration 1 every second
+        print(timerOn)
+        print(timeRemaining)
+        if (timerOn == true) {
+            countDown()
+        }
     }
     
-    @objc func updateCounter() {
-        
-        
-        
+    func countDown() {
+        timeRemaining -= 1
+        remainingTimeLabel.text = "Time Remaining: " + String(format: "%02d", timeRemaining / 3600) + ":" + String(format: "%02d", (timeRemaining % 3600) / 60) + ":" + String(format: "%02d", (timeRemaining % 3600) % 60)
+        if (timeRemaining == 0 && timerOn == true) {
+            timerOver()
+        }
     }
 
+    func timerOver() {
+        timerOn = false
+        playSound()
+        timerButton.setTitle("Stop Music", for: .normal)
+    }
+    
+    func changeBackground() {
+        let hour  = Calendar.current.component(.hour, from: Date())
+        if (hour < 12) {
+            self.view.backgroundColor = .cyan
+        } else {
+            self.view.backgroundColor = .lightGray
+        }
+    }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        if (player != nil) {
+            player.stop()
+            timerButton.setTitle("Start Timer", for: .normal)
+            player = nil
+        } else if (timerOn == false) {
+            timerOn = true
+            timerButton.setTitle("Stop Timer", for: .normal)
+            timeRemaining = Int(timePicker.countDownDuration)
+        } else if (timerOn == true) {
+            timerOn = false
+            timerButton.setTitle("Start Timer", for: .normal)
+        }
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "audio_sample", withExtension: "mp3")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+    }
 }
 
